567abde6a8355f350393d6f60ec590a12e2b9559
KaiqueWilliam <kaiquewilliamsilva@gmail.com>
2023-12-02 22:59:59 -0300

    update ValidarSolicitacao @KaiqueWilliam,

--- src/Requerimentos/ValidarSolicitacao.java
+++ src/Requerimentos/ValidarSolicitacao.java
@@ -44,14 +44,32 @@ public class ValidarSolicitacao  {
     }
     public void validaProfessor(Boolean statusProfessor, String disciplinaUser, String professorUser) {
         List<Professores> listaProfessores = base.getProfessores();
-        if (!statusProfessor)
-            System.out.println(SituacaoReserva.REPROVADO);
-        else {
-            System.out.println(SituacaoReserva.APROVADO);
+        
+        switch (professorUser) {
+            case "Professor01", "Professor02", "Professor03", "Professor04", "Professor05", "Professor06", "Professor07", "Professor08", "Professor09", "Professor010" -> {
+                if (disciplinaUser != "BES005" || disciplinaUser != "BES006" || disciplinaUser != "BES008") {
+                    System.out.println(SituacaoReserva.REPROVADO);
+                } else {
+                    System.out.println(SituacaoReserva.APROVADO);
+                }
+            }
+            case "Professor011", "Professor012", "Professor013", "Professor014" -> {
+                if (disciplinaUser != "BES011" || disciplinaUser != "BES012" || disciplinaUser != "BES020" || disciplinaUser != "BES026") {
+                    System.out.println(SituacaoReserva.REPROVADO);
+                } else {
+                    System.out.println(SituacaoReserva.APROVADO);
+                }
+            }
+            case "Professor015" -> {
+               if (disciplinaUser != "BES038" || disciplinaUser != "BES049" || disciplinaUser != "BES048") {
+                    System.out.println(SituacaoReserva.REPROVADO);
+                } else {
+                    System.out.println(SituacaoReserva.APROVADO);
+                    
+                }
+    
+            }
         }
-
-
-
 
     }
     public void validaAlunos(Boolean statusAlunos, List<Alunos> grupoAlunos) {
